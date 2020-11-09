efdc9751f05b38a64d742667a43af5f1a309a0a8
Jayden <jayden.gilmore@centre.edu>
2020-11-08 20:58:43 -0500

    Finished order list

--- classes.js
+++ classes.js
@@ -1,6 +1,6 @@
 var directoryPromise = d3.json("classData.json");
-var getIMG = function(image){
-    return "imgs/"+image.picture;
+var getIMG = function (image) {
+    return "imgs/" + image.picture;
 }
 
 var quizMeans = function(penguin){
@@ -36,13 +36,8 @@ var clearTable = function() {
     .remove();
 }
 
-
-
-
-var successFCN = function(penguins){
-    console.log(penguins);
-    
-    var rows = d3.select("tbody")
+var drawTable = function(penguins){
+    var rows = d3.select(" tbody")
     .selectAll("tr")
     .data(penguins)
     .enter()
@@ -50,23 +45,56 @@ var successFCN = function(penguins){
     
     rows.append("td")
     .append("img")
+    .attr("class", "pict")
     .attr("src", getIMG);
     
     rows.append("td")
+    .attr("class", "quiz")
     .text(quizMeans);
     
     rows.append("td")
+    .attr("class", "hw")
     .text(hwMeans);
     
-    rows.append("td")
+      rows.append("td")
+    .attr("class", "test")
     .text(testMean);
     
-    rows.append("td")
+      rows.append("td")
     .attr("class", "finalGrade")
     .text(finalMean);
-    
+};
+
+var clearTable = function() {
+    d3.selectAll("tbody tr")
+    .remove();
 }
 
+var initHeaders = function(penguins){
+    d3.select("#finalGrade")
+    .on("click", function(){
+        console.log("Clicked Quiz");
+        penguins.sort(function(a,b) {
+            var av1 = finalMean(a);
+            var av2 = finalMean(b);
+            if (av1 < av2) {return 1;}
+            else if (av1 > av2) {return -1;}
+            else {return 0;}
+        });
+        clearTable();
+        drawTable(penguins)
+        d3.selectAll(".finalGrade")
+        .attr("class", "selected");
+    });
+}
+
+
+var successFCN = function(penguins){
+    console.log("data", penguins);
+    drawTable(penguins);
+    initHeaders(penguins)
+}
+    
 var failFCN = function(errMessage){
     console.log("failure", errMessage);
     d3.selectAll("h1")
